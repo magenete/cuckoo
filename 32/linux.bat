a57edde1a10d60dcf8f608c602daf9feeb8de5ce
che <che.pasha@gmail.com>
2016-03-08 01:49:44 +0100

    New structure has been defined

--- 32/linux.bat
+++ 32/linux.bat
@@ -6,27 +6,29 @@ set CUCKOO_CPU_CORES=4
 set CUCKOO_CURRENT_DIR=%~dp0
 set CUCKOO_TMP_DIR=%TEMP%
 
+set QEMU_NAME=qemu
 set QEMU_VERSION=2.5.0
-set QEMU_DIR=qemu
-set QEMU_RUN_DIR=%CUCKOO_CURRENT_DIR%%QEMU_DIR%\windows\%QEMU_VERSION%
-set QEMU_BIN_FILE=\qemu-system-i386
+set QEMU_RUN_DIR=%CUCKOO_CURRENT_DIR%%QEMU_NAME%\%CUCKOO_OS%\%QEMU_VERSION%
+set QEMU_BIN_FILE=\%QEMU_NAME%-system-i386.exe
 
 
-"%QEMU_RUN_DIR%%QEMU_BIN_FILE%" -version > nul
-if %ERRORLEVEL% == 0 (
-    mkdir "%CUCKOO_TMP_DIR%\%QEMU_DIR%\%QEMU_VERSION%" > nul
-    xcopy /E /C /I /R /Y  "%QEMU_RUN_DIR%" "%CUCKOO_TMP_DIR%\%QEMU_DIR%\%QEMU_VERSION%"
-    set QEMU_RUN_DIR=%CUCKOO_TMP_DIR%\%QEMU_DIR%\%QEMU_VERSION%
-)
+rem "%QEMU_RUN_DIR%%QEMU_BIN_FILE%" -version > nul
+rem if %ERRORLEVEL% == 0 (
+    mkdir "%CUCKOO_TMP_DIR%\%QEMU_NAME%\%QEMU_VERSION%" > nul
+    xcopy /E /C /I /R /Y "%QEMU_RUN_DIR%" "%CUCKOO_TMP_DIR%\%QEMU_NAME%\%QEMU_VERSION%"
+    set QEMU_RUN_DIR=%CUCKOO_TMP_DIR%\%QEMU_NAME%\%QEMU_VERSION%
+rem )
 
+cd "%QEMU_RUN_DIR%"
 "%QEMU_RUN_DIR%%QEMU_BIN_FILE%" ^
-    -name "Cuckoo -- Windows [%CUCKOO_OS_BIT%]" ^
+    -L %QEMU_RUN_DIR% ^
+    -name " Cuckoo -- Linux [%CUCKOO_OS_BIT%] " ^
     -boot order=c ^
     -drive media=disk,index=0,file=%CUCKOO_CURRENT_DIR%hd\%CUCKOO_OS%\0 ^
     -drive media=disk,index=1,file=%CUCKOO_CURRENT_DIR%hd\%CUCKOO_OS%\1 ^
     -drive media=disk,index=2,file=%CUCKOO_CURRENT_DIR%hd\%CUCKOO_OS%\2 ^
     -drive media=disk,index=3,file=%CUCKOO_CURRENT_DIR%hd\%CUCKOO_OS%\3 ^
-    -drive media=disk,index=4,file=%CUCKOO_CURRENT_DIR%hd\%CUCKOO_OS%\4 ^
     -m 1G ^
-    -cpu "%QEMU_DIR%%CUCKOO_OS_BIT%" -smp %CUCKOO_CPU_CORES%,cores=%CUCKOO_CPU_CORES%,maxcpus=%CUCKOO_CPU_CORES% ^
+    -sdl -display sdl ^
+    -cpu "%QEMU_NAME%%CUCKOO_OS_BIT%" -smp %CUCKOO_CPU_CORES%,cores=%CUCKOO_CPU_CORES%,maxcpus=%CUCKOO_CPU_CORES% ^
     -usb -usbdevice tablet
--- 64/linux.sh
+++ 64/linux.sh
@@ -5,18 +5,18 @@ CUCKOO_CPU_CORES=4
 CUCKOO_CURRENT_DIR="$(realpath $(readlink -f $(dirname $0)))"
 CUCKOO_TMP_DIR="${TMPDIR:=/tmp}/"
 
-QEMU_DIR="qemu"
+QEMU_NAME="qemu"
 QEMU_VERSION="2.5.0"
-QEMU_RUN_DIR="${CUCKOO_CURRENT_DIR}/${QEMU_DIR}/linux/${QEMU_VERSION}"
-QEMU_BIN_FILE="/bin/qemu-system-x86_64"
+QEMU_RUN_DIR="${CUCKOO_CURRENT_DIR}/${QEMU_NAME}/${CUCKOO_OS}/${QEMU_VERSION}"
+QEMU_BIN_FILE="/bin/${QEMU_NAME}-system-x86_64"
 
 
 ${QEMU_RUN_DIR}${QEMU_BIN_FILE} -version > /dev/null
 if [ ! $? -eq 0 ]
 then
-    mkdir -p ${CUCKOO_TMP_DIR}/${QEMU_DIR}
-    cp -rf $QEMU_RUN_DIR $CUCKOO_TMP_DIR/${QEMU_DIR}
-    QEMU_RUN_DIR="${CUCKOO_TMP_DIR}/${QEMU_DIR}/${QEMU_VERSION}"
+    mkdir -p ${CUCKOO_TMP_DIR}/${QEMU_NAME}
+    cp -rf $QEMU_RUN_DIR $CUCKOO_TMP_DIR/${QEMU_NAME}
+    QEMU_RUN_DIR="${CUCKOO_TMP_DIR}/${QEMU_NAME}/${QEMU_VERSION}"
     chmod -R 750 $QEMU_RUN_DIR
 fi
 
@@ -27,11 +27,9 @@ ${QEMU_RUN_DIR}${QEMU_BIN_FILE} \
     --drive media=disk,if=scsi,index=1,file=${CUCKOO_CURRENT_DIR}/hd/${CUCKOO_OS}/1 \
     --drive media=disk,if=scsi,index=2,file=${CUCKOO_CURRENT_DIR}/hd/${CUCKOO_OS}/2 \
     --drive media=disk,if=scsi,index=3,file=${CUCKOO_CURRENT_DIR}/hd/${CUCKOO_OS}/3 \
-    --drive media=disk,if=scsi,index=4,file=${CUCKOO_CURRENT_DIR}/hd/${CUCKOO_OS}/4 \
     -m 1G \
-    -cpu "${QEMU_DIR}${CUCKOO_OS_BIT}" -smp ${CUCKOO_CPU_CORES},cores=${CUCKOO_CPU_CORES},maxcpus=${CUCKOO_CPU_CORES} \
+    -cpu "${QEMU_NAME}${CUCKOO_OS_BIT}" -smp ${CUCKOO_CPU_CORES},cores=${CUCKOO_CPU_CORES},maxcpus=${CUCKOO_CPU_CORES} \
     -vga std \
     -usb -usbdevice tablet \
     -enable-kvm \
     -daemonize
-#-cdrom ./linux/${CUCKOO_OS_BIT}/hd/debian-8.3.0-amd64-netinst.iso \
