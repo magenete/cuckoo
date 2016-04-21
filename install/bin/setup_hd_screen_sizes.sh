0b2cf258db694d3ffce5d671a03c5f81e708cf26
magenete <magenete.systems@gmail.com>
2016-04-21 08:31:34 +0200

    Screen size setting has been optimized

--- install/bin/setup_hd_screen_sizes.sh
+++ install/bin/setup_hd_screen_sizes.sh
@@ -1,13 +1,26 @@
 
-HD_SCREEN_SIZE_LIST="1280x720 1600x900 1920x1080"
+HD_SCREEN_SIZE_LIST_DEFAULT="1920x1080 1600x900 1280x720"
+HD_SCREEN_HZ=60
 
+
+if [ -z "$1" ]
+then
+    HD_SCREEN_SIZE_LIST="$HD_SCREEN_SIZE_LIST_DEFAULT"
+else
+    HD_SCREEN_SIZE_LIST="$1"
+fi
+
+
+echo -n "Screen size setting...  "
 
 for screen_size in $HD_SCREEN_SIZE_LIST
 do
     xrandr | grep $screen_size > /dev/null
     if [ $? -gt 0 ]
     then
-        GTF_DATA="$(gtf $(echo $screen_size | tr 'x' ' ') 60 | grep Modeline | cut -d\" -f3 2> /dev/null)"
+        echo -n "$screen_size "
+
+        GTF_DATA="$(gtf $(echo $screen_size | tr 'x' ' ') $HD_SCREEN_HZ | grep Modeline | cut -d\" -f3 2> /dev/null)"
         if [ ! -z "$GTF_DATA" ]
         then
             xrandr --newmode "$screen_size" $GTF_DATA
@@ -15,3 +28,9 @@ do
         fi
     fi
 done
+
+echo -n "  ...are completed!"
+echo ""
+
+
+exit 0
